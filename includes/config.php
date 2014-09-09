ba8f30fd8e1721f5a3223772394d6f94481af94e
Utsav Narayan Singh <vastusingh@gmail.com>
2014-09-09 16:59:08 +0530

    Cleaning up config.

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "127.0.0.1");
 defined('DB_USER')   ? null : define("DB_USER", "root");
-defined('DB_PASS')   ? null : define("DB_PASS", "allinthemind");
+defined('DB_PASS')   ? null : define("DB_PASS", "YOUR PASS HERE");
 defined('DB_NAME')   ? null : define("DB_NAME", "bmdb");
 
 ?>
